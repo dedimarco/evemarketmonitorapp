1c51881f15e61b920e7b6b7b3138ca01753ceda2
SteveBallantine <SteveBallantine@51e56510-333b-11de-8e0c-53a27eea117e>
2010-04-16 09:07:57 +0000

    BETA release 1.4.1.3
    Changes to SQL detection process
    Hide splash screen if error occur during startup otherwise the error is hidden by the splash screen.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.1.2")]
-[assembly: AssemblyFileVersion("1.4.1.2")]
+[assembly: AssemblyVersion("1.4.1.3")]
+[assembly: AssemblyFileVersion("1.4.1.3")]
